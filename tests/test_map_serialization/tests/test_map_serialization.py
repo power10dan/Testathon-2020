25f2de3ce335afa79d3823f37c9d554eff1d13ed
Daniel <dlin@ds.dfci.harvard.edu>
2020-02-19 13:58:53 -0500

    Tests For write_map and read_map

---
+++ tests/test_map_serialization/tests/test_map_serialization.py
@@ -0,0 +1,10 @@
+def test_map_literal(workflow_data, workflow_runner):
+    expected = workflow_data.get_dict( "out_file_str", "out_file_int", "out_file_bool")
+    input={}
+    workflow_runner(
+        "../main.wdl",
+        input,
+        expected,
+        workflow_name="wf",
+    )
+
